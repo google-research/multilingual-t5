86a3135977446d5e67e4133440aaaf5b38b943eb
Sharan Narang <sharannarang@google.com>
2022-09-29 12:17:45 -0700

    Fix pawsx translate train original mixture
    PiperOrigin-RevId: 477789615

--- multilingual_t5/tasks.py
+++ multilingual_t5/tasks.py
@@ -320,7 +320,7 @@ pawsx_translate_train_original = [
 ] + pawsx_eval
 seqio.MixtureRegistry.add(
     "mt5_pawsx_translate_train_original",
-    pawsx_translate_train,
+    pawsx_translate_train_original,
     default_rate=1.0)
 
 # ----- TyDiQA GoldP-----
